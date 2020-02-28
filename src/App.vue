317ec15e3aaefd4b4de91340ff56c8960984998f
e <515418500@qq.com>
2020-02-28 16:20:56 +0800

    '动态切屏'

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,27 @@
 import './App.less';
 export default {
   data() {
-  }
+    return {
+      index: 0,
+      data: [
+        'mainPage',
+        'smartInd',
+        'cockpit',
+        'device',
+        'prodCtrl',
+        'safe'
+      ]
+    };
+  },
+  mounted() {
+    window.location.href = `http://localhost:8080/#/${this.data[this.index]}`;
+    setInterval(() => {
+      this.index += 1;
+      if (this.index > 5) {
+        this.index = 0;
+      }
+      window.location.href = `http://localhost:8080/#/${this.data[this.index]}`;
+    }, 10 * 1000);
+  },
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -1,57 +1,64 @@
 // @ts-nocheck
-import Vue from "vue";
-import Router from "vue-router";
-import index from "./routes/index";
-import mainPage from "./routes/mainPage";
-import device from "./routes/device";
-import cockpit from "./routes/cockpit";
-import parkCommand from "./routes/parkCommand";
+import Vue from 'vue';
+import Router from 'vue-router';
+import index from './routes/index';
+import mainPage from './routes/mainPage';
+import device from './routes/device';
+import cockpit from './routes/cockpit';
 import ProductionControl from './routes/productionControl'
 import Safe from './routes/safe'
 import SmartInd from './routes/smartInd'
 Vue.use(Router);
-
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "index",
+      path: '/',
+      name: 'index',
       component: index
     },
     {
       path: '/prodCtrl',
       name: 'prodCtrl',
+      meta: {
+        title: '工厂生产操作管理驾驶舱'
+      },
       component: ProductionControl,
     },
     {
       path: '/safe',
       name: 'safe',
+      meta: {
+        title: '工厂安全环保管理驾驶舱'
+      },
       component: Safe,
     },
     {
       path: '/smartInd',
       name: 'SmartInd',
+      meta: {
+        title: '智慧工业园区运维驾驶舱'
+      },
       component: SmartInd,
     },
     {
-      path: "/mainPage",
-      name: "mainPage",
+      path: '/mainPage',
+      name: 'mainPage',
       meta: {
-        title: '多地协同集团全景驾驶舱 '
+        title: '集团异地业务协同全景驾驶舱'
       },
       component: mainPage
     },
     {
-      path: "/device",
-      name: "device",
+      path: '/device',
+      name: 'device',
       meta: {
-        title: '工厂设备运行管理驾驶舱 '
+        title: '工厂设备运行管理驾驶舱'
       },
       component: device
     },
     {
-      path: "/cockpit",
-      name: "cockpit",
+      path: '/cockpit',
+      name: 'cockpit',
       meta: {
         title: '智能互联工厂管控驾驶舱'
       },
